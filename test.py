cbf19c71ab104e760c5ecf5b9c7694ce0c19a5be
githubpro101 <86120858+githubpro101@users.noreply.github.com>
2021-07-05 21:38:56 -0400

    Add files via upload
    1. Created Virtual environment named ‘selenium’ with “python -m venv selenium” command in terminal
    2. pip installed selenium
    started with a base code from "https://selenium-python.readthedocs.io/getting-started.html"

---
+++ test.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(executable_path=r'C:\Users\Jarvis\Desktop\New folder\Python\selenium\chromedriver.exe')
+driver.get("http://www.python.org")
