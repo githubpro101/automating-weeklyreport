049359282ba11ec4349692c7418588e20fcc723a
githubpro101 <86120858+githubpro101@users.noreply.github.com>
2021-07-06 23:05:12 -0400

    Add files via upload
    this is piece of function which was implemented in test4.py trying out str. function, while working to get just day out of current date

---
+++ test4.1.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from datetime import date
+today = date.today().day
+# todaysdate = today.strftime("%m/%d/%y")
+datenow=str(today)
+print (datenow)
